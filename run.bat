7755e4e421b7d0f7eff7384420e665fb58f60768
Jake Coxon <jake.coxon@gmail.com>
2012-12-06 23:09:02 +0000

    Deleted items for release

--- run.bat
+++
@@ -1,2 +0,0 @@
-C:\moterunner\win32\bin\mrc.exe --verbose --assembly=radio-red-1.0 src/embs/RadioRed.java -r:logger-9.0
-C:\moterunner\win32\bin\mrc.exe --verbose --assembly=radio-green-1.0 src/embs/RadioGreen.java -r:logger-9.0
